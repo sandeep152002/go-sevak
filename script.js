682dbbd333f04b7ed03fe63ebaf277c4d9ded849
sandeep152002 <94821752+sandeep152002@users.noreply.github.com>
2025-04-14 12:37:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! We will contact you soon.");
+  });
+  
